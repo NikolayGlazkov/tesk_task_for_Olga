23f08e92d11edebfb92afe480967dccb173040de
NikolayG <gn9377419582@gmail.comm>
2024-08-01 12:08:52 +0300

    fix

--- task4/task4.py
+++ task4/task4.py
@@ -6,14 +6,9 @@ def min_moves(nums):
 
 def read_data(filename):
     with open(filename, 'r') as file:
-        # Читаем все строки из файла
         lines = file.readlines()
-    
-    # Преобразуем каждую строку в целое число и создаем список чисел
     numbers = [int(line.strip()) for line in lines]
     
     return numbers
-# nums = [1,10, 2, 9,]
-# print(f"Минимальное количество ходов: {min_moves(nums)}")  # Вывод: 2
 
 print(min_moves(read_data("data.txt")))

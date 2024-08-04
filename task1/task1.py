31309bdf7eff593cca20322134c1c66d2b40d7c4
NikolayG <gn9377419582@gmail.comm>
2024-08-04 12:11:36 +0300

    ferst end

--- task1/task1.py
+++ task1/task1.py
@@ -1,13 +1,19 @@
-def cercle_aray(n,m):
+import argparse
+
+def cercle_aray(n, m):
     i = 1
     while True:
-        print(i, end='')
+        print(i, end=' ')
         i = 1 + (i + m - 2) % n
         if i == 1:
             break
     print()
 
-
-n = int(input("Введите n = "))
-m = int(input("Введите m = "))
-cercle_aray(n,m)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Печать последовательности чисел в цикле с заданным шагом.")
+    parser.add_argument('n', type=int, help='Количество элементов в последовательности.')
+    parser.add_argument('m', type=int, help='Шаг для перехода к следующему элементу.')
+    
+    args = parser.parse_args()
+    
+    cercle_aray(args.n, args.m)
